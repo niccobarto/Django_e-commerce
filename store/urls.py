587b0f3ca00f53dbe78bff73c393bf3086f3b331
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-06-13 16:33:00 +0200

    Row version of product page

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('product/<int:product_id>', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def home(request):
     products=Product.objects.all()
     return render(request, 'home.html', {"products":products})
+
+def product_detail(request,product_id):
+    product=Product.objects.get(pk=product_id)
+    return render(request, 'product_detail.html', {"product":product})
