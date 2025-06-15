30a8bc997e6214f51243b21dad50552cbc53281e
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-06-15 11:30:38 +0200

    Implemented templates separations with base.html and navbar.html
    Add login form and functionality

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/<int:product_id>', views.product_detail, name='product_detail'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
 ]
