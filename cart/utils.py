a1d5757975795663563c03aa1778c29e25e2faa6
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-07-03 18:51:01 +0200

    Resolved confirm order issue

--- cart/utils.py
+++ cart/utils.py
@@ -6,7 +6,7 @@ def quantity_price(price, quantity):
 def cart_total_price(cart_items):
     total=0
     for item in cart_items:
-        total += quantity_price(item.product.discounted_price(), item.quantity)
+        total += quantity_price(item.product.discounted_price, item.quantity)
     return total
 
 def cart_total_items(cart_items):
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from pyexpat.errors import messages
 from django.contrib import messages
 from store.models import Product
 from .models import CartItem
