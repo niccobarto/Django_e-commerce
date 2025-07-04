57d92a24dcf01846021a149dca02ef786b3e4d7a
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-07-04 10:55:57 +0200

    Changes for work also in local

--- djangoEcommerce/settings.py
+++ djangoEcommerce/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = "djangoEcommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = config('DATABASE_URL', default='postgresql://postgres:Anotherunifithing@localhost:5432/ecommerce_db')
-
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Anotherunifithing@localhost:5432/ecommerce_db',
+        conn_max_age=600
+    )
 }
-
 AUTH_USER_MODEL = 'accounts.CustomUser'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
