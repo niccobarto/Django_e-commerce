0c1c58389b1fb42a946bedcd39411dc72e457527
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-07-09 10:06:50 +0200

    For have big description text area in ProductForm

--- store/forms.py
+++ store/forms.py
@@ -5,3 +5,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields=['name','category','price','description','front_image','quantity','is_active','discount']
+        widgets = {
+            'description': forms.Textarea(attrs={
+                'rows':5,
+            }),
+        }
