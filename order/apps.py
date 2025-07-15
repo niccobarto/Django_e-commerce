4264272df0c58d43c6faeeec266a5454efa8307b
Niccolò Bartoli <niccolobartoli18@gmail.com>
2025-07-15 17:28:02 +0200

    Code for update orderitems with price error

--- order/apps.py
+++ order/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+import os
+import logging
 
 class OrderConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "order"
+
+    def ready(self):
+        # Controllo di sicurezza: esegui solo se è settata una variabile
+        if os.environ.get("RUN_FIX_ORDERITEMS", "false") == "true":
+            from order.models import OrderItem  # importa il modello dentro ready()
+            updated = 0
+
+            for item in OrderItem.objects.all():
+                if item.product:
+                    item.price = item.product.price
+                    item.save()
+                    updated += 1
+
+            logging.warning(f"[FIX_ORDERITEMS] Aggiornati {updated} OrderItem.")
